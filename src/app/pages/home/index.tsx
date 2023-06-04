5a137df44a331ea825ff1f1dfac2e6caf953f08c
Vũ Trung Nghĩa <vutrungnghiahn@gmail.com>
2023-06-04 11:16:00 +0700

    feat: dashboard

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,3 +1,31 @@
-const Home = (): JSX.Element => <></>;
+/* eslint-disable */
+import moment from 'moment';
+import { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const Home = () => {
+  const [events] = useState([
+    {
+      start: moment().toDate(),
+      end: moment().add(1, 'days').toDate(),
+      title: 'Some title',
+    },
+  ]);
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={events}
+        style={{ height: '100vh' }}
+      />
+    </div>
+  );
+};
 
 export default Home;
